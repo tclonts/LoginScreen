b8d24b7dc47d56c9c257cc0e85799cc230040471
tylerclonts <tclonts77@gmail.com>
2018-03-12 14:48:28 -0600

    Updated login to have a control state and updated assets

--- CreateLoginScreen/LoginViewController.swift
+++ CreateLoginScreen/LoginViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Properties
+    var buttonState: ButtonState = .notSelected
+    
+    enum ButtonState {
+        case selected
+        case notSelected
     }
+    
+    // MARK: - Outles
+    @IBOutlet weak var headerImageViw: UIImageView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var aboutMeLabel: UILabel!
+    @IBOutlet weak var loginLabel: UIButton!
+    @IBOutlet weak var lineView: UIView!
+    
+    
+    // MARK: - Actions
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        updateViews()
+    }
+    
+    func setUpUI() {
+        
+        // This function is used to match our sketch Design(UI)
+        headerImageViw.image = #imageLiteral(resourceName: "Raven")
+        headerImageViw.contentMode = .scaleAspectFit
+        profileImageView.image = #imageLiteral(resourceName: "Profile")
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.cornerRadius = profileImageView.layer.frame.height / 2
+        profileImageView.layer.borderColor = UIColor.white.cgColor
+        profileImageView.layer.borderWidth = 5
+        
+        // Button UI
+        loginLabel.layer.cornerRadius = 10
+        loginLabel.clipsToBounds = true
+        loginLabel.backgroundColor = UIColor.primaryBlue
+        loginLabel.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        loginLabel.layer.borderWidth = 2
+        loginLabel.setTitleColor(.white, for: .normal)
+        
+        // LineView
+        
+        lineView.backgroundColor = .black
+    }
+    
+    func updateViews() {
+        switch buttonState {
+            
+        case .selected:
+            buttonState = .notSelected
+            loginLabel.setTitle("Login", for: .normal)
+        case .notSelected:
+            buttonState = .selected
+            loginLabel.setTitle("Update", for: .normal)
+        }
+        
+    }
+    
     
 
     /*
---
+++ CreateLoginScreen/Theme.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  CreateLoginScreen
+//
+//  Created by Tyler Clonts on 3/12/18.
+//  Copyright © 2018 Tyler Clonts. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static var primaryBlue: UIColor {
+        return UIColor(red: 1.0/255.0, green: 98.0/255.0, blue: 202.0/255.0, alpha: 1.0)
+    }
+    
+    
+}
